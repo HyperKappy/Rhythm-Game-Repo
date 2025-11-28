afc530c45b5b01c9e709177d41472eb4517bd249
flaw01 <t.korsman@outlook.com>
2025-11-28 17:26:27 +0100

    debug codes weghalen
    je kan nu niet meer op 1 en 2 drukken om de levels opnieuw te laden tijdens het spelen

--- code/game_level.gd
+++ code/game_level.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and not event.echo:
-		if event.keycode == KEY_1:
-			_start_guess()
-		if event.keycode == KEY_2:
-			_start_enough()
+#func _input(event: InputEvent) -> void:
+#	if event is InputEventKey and event.pressed and not event.echo:
+#		if event.keycode == KEY_1:
+#			_start_guess()
+#		if event.keycode == KEY_2:
+#			_start_enough()
 
 func _start_guess() -> void:
 	get_tree().change_scene_to_file("res://levels/guess.tscn")
