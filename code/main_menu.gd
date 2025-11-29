4b1cf2742c2b3043993351c5e369db3734c0be90
flaw01 <t.korsman@outlook.com>
2025-11-29 13:43:29 +0100

    temp levels toegevoegd
    tijdelijke levels die ik even uit quaver heb geplukt zodat testpersonen ieerst iets makkelijkere dingen kunnen proberen voordat ze guess doen

---
+++ code/main_menu.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and not event.echo:
+		if event.keycode == KEY_1:
+			_start_temp1()
+		if event.keycode == KEY_2:
+			_start_temp2()
+		if event.keycode == KEY_3:
+			_start_temp3()
+
+func _start_temp1() -> void:
+	get_tree().change_scene_to_file("res://levels/temp1.tscn")
+	
+func _start_temp2() -> void:
+	get_tree().change_scene_to_file("res://levels/temp2.tscn")
+
+func _start_temp3() -> void:
+	get_tree().change_scene_to_file("res://levels/temp3.tscn")
