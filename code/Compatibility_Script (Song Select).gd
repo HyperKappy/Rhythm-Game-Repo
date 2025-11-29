5819c234d2e692f5f9ac4c8f3b88d066ad64f18c
flaw01 <t.korsman@outlook.com>
2025-11-29 18:01:38 +0100

    secret limbo toegevoegd
    druk in de song select de toetsen L, I, M, B en O in tegelijkertijd

--- code/Compatibility_Script (Song Select).gd
+++ code/Compatibility_Script (Song Select).gd
@@ -1,9 +1,27 @@
 extends Node2D
 
-func _input(_event):
-	if Input.is_key_pressed(KEY_ESCAPE):
-		get_tree().quit()
+var required_keys = {
+	KEY_L: false,
+	KEY_I: false,
+	KEY_M: false,
+	KEY_B: false,
+	KEY_O: false,
+}
 
+func _input(event: InputEvent):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().change_scene_to_file("res://scenes/Main menu.tscn")
+	if event is InputEventKey:
+		if event.keycode in required_keys:
+			required_keys[event.keycode] = event.pressed
+			if _all_keys_pressed():
+				get_tree().change_scene_to_file("res://levels/limbo_hard.tscn")
+
+func _all_keys_pressed() -> bool:
+	for pressed in required_keys.values():
+		if pressed == false:
+			return false
+	return true
 
 func _on_Pou_Pressed_connect() -> void:
 	await get_tree().create_timer(1.0).timeout
--- code/main_menu.gd
+++ code/main_menu.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and not event.echo:
 		if event.keycode == KEY_1:
