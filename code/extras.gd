a9570ed38676acab01552494954f1819dc32fd39
HyperKappy <105985950+HyperKappy@users.noreply.github.com>
2025-12-09 12:14:23 +0100

    Image fixen lol
    Daarnaast ook wat preloaden voor performance redenen (het doet vrij weinig maar het kan vast geen kwaad).
    Daarnaast wat rudimentaire plaatjes verwijderen

--- code/extras.gd
+++ code/extras.gd
@@ -4,5 +4,7 @@ extends TextureButton
 
 func _on_pressed() -> void:
 	$Click_Animation.play("Schuiven")
-	await get_tree().create_timer(1.0).timeout
+	preload("res://scenes/Extras.tscn")
+	preload("res://Resources/images/Extras_Menu.png")
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://scenes/Extras.tscn")
