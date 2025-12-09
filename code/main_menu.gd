7df2396450afbb9e1d3a79d96596d96c713a4a21
flaw01 <t.korsman@outlook.com>
2025-12-09 12:29:13 +0100

    debug en test levels weggehaald

--- code/main_menu.gd
+++ code/main_menu.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and not event.echo:
-		if event.keycode == KEY_1:
-			_start_temp1()
-		if event.keycode == KEY_2:
-			_start_temp2()
-		if event.keycode == KEY_3:
-			_start_temp3()
+#func _input(event: InputEvent) -> void:
+#	if event is InputEventKey and event.pressed and not event.echo:
+#		if event.keycode == KEY_1:
+#			_start_temp1()
+#		if event.keycode == KEY_2:
+#			_start_temp2()
+#		if event.keycode == KEY_3:
+#			_start_temp3()
 #		if event.keycode == KEY_0:
 #			_start_test()
 
